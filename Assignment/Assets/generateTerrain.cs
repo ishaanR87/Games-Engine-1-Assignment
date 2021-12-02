dc7bc11d5877e3e9d0864d97771286357cf60900
ishaan rawat <c18459302@mytudublin.ie>
2021-12-02 13:12:37 +0000

    removed terrain, added plane and wrote up a script to lift vertices of plane

---
+++ Assignment/Assets/generateTerrain.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// to initalize height and detail of the landscape
+int height = 5;
+float detail = 5.0f;
+
+public class generateTerrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // mesh is taken from the object attached to this 
+        Mesh mesh = this.getComponent<MeshFilter>().mesh;
+        Vector3 [] vertices = mesh.vertices;
+
+        // looping over mesh
+        for(int v = 0; v < vertices.Length; v++)
+        {
+            // lifting vertices by taking y position of each vertices in the mesh
+            // perlin noise lifts taking values from x and y position of the vertex
+            // using position of the plane as offset for function so plane isnt repeated entirely            vertices[v].y = Mathf.PerlinNoise((vertices[v].x + this.transform.position.x)/detail,
+                                               (vertices[v].z + this.transform.position.z)/detail)*height;                                  
+        }
+
+        // setting calculated vertices into mesh
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        // to explore
+        this.gameObject.AddComponent<MeshCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
