4dc51b4900f59b22a43c0fc51e23682a5b6f811f
ishaan rawat <c18459302@mytudublin.ie>
2021-12-06 02:54:06 +0000

    getTree function added in treeBuild file. Also, variables set to static

--- Assignment/Assets/treeBuild.cs
+++ Assignment/Assets/treeBuild.cs
@@ -6,21 +6,36 @@ public class treeBuild : MonoBehaviour
 {
 
     // number of trees being generated
-        int numTrees = 100;
+        static int numTrees = 100;
         // from this object
         public GameObject treePrefab;
         // array of trees
-        GameObject[] trees;
+       static GameObject[] trees;
 
     // Start is called before the first frame update
     void Start()
     {   
+        // set array to be size of numtrees to allocate memory
         trees = new GameObject[numTrees];
         for(int i = 0; i< numTrees; i++)
         {
             trees[i] = (GameObject) Instantiate(treePrefab, Vector3.zero, Quaternion.identity);
+            // trees made inactive
             trees[i].SetActive(false);
         }
+    }
+    	
+    // loops through trees to look for unactive trees and will return them
+    static public GameObject getTree()
+    {
+        for(int i = 0; i < numTrees; i++)
+        {
+            if(!trees[i].activeSelf)
+            {
+                return trees[i];
+            }
+        }
+        return null;
     }
 
     // Update is called once per frame
