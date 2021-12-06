bf0ce3b9b99a70c6acb6e476cc50184dd3ac06ad
ishaan rawat <c18459302@mytudublin.ie>
2021-12-06 02:46:07 +0000

    tree cloning generated but all under map under xame x and z axis

--- Assignment/Assets/treeBuild.cs
+++ Assignment/Assets/treeBuild.cs
@@ -8,14 +8,19 @@ public class treeBuild : MonoBehaviour
     // number of trees being generated
         int numTrees = 100;
         // from this object
-        public GameObject = treePrefab;
+        public GameObject treePrefab;
         // array of trees
         GameObject[] trees;
 
     // Start is called before the first frame update
     void Start()
     {   
-
+        trees = new GameObject[numTrees];
+        for(int i = 0; i< numTrees; i++)
+        {
+            trees[i] = (GameObject) Instantiate(treePrefab, Vector3.zero, Quaternion.identity);
+            trees[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
